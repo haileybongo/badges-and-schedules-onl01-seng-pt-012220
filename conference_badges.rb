f7e9bb636c6345f9371ff75d60e7c57244cb327d
Hailey Bongo <haileyebongo@gmail.com>
2020-01-28 23:01:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,7 @@ def batch_badge_creator(attendees)
   # attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each {|name| return "Hello, my name is #{name}."]
 end
+end
 
 def assign_rooms(speaker_array)
   room = 1
