8462290f2b97d9a8d7d780615179623904269dee
Hailey Bongo <haileyebongo@gmail.com>
2020-01-28 22:22:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.each do |names| 
+  return "Hello, my name is #{names}."
+end
+end
+
+def assign_rooms(speaker_array)
+  room = 1
+  room_assign = []
+  while room < 8
+  speaker_array.each do |speaker|
+  assignment = "Hello #{speaker}! You'll be assigned to room #{room}!"
+  room_assign << assignment
+  room += 1
+    end
+    end
+  return room_assign
+end
